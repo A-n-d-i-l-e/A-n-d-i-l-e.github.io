82fb6ff78e5d33fd97b86110329e1e8a0bc9dbe9
A-n-d-i-l-e <ndlovuandile100@gmail.com>
2024-02-21 03:04:27 +0200

    .

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -78,7 +78,7 @@ const CONFIG = {
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      'https://drive.google.com/file/d/1uwnOS51bG6YHyu0BpbgGM8x_1RwMwMEd/view?usp=sharing', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
     'Typescript',
